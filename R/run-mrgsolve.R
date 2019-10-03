821586c6c44f23e95c9e58287fe749043dfb601c
Evgeny Metelkin <metelkin@insysbio.com>
2019-10-03 20:35:06 +0300

    add diagnostics

---
+++ R/run-mrgsolve.R
@@ -0,0 +1,30 @@
+### settings
+model_path <- './dist/mrg.cpp'
+fig_path <- './dist/mrg.png'
+library(mrgsolve)
+
+### main code
+message('Reading model... ', appendLF = FALSE)
+model <- mrgsolve::mread(model = 'default', file = model_path)
+message('OK')
+
+message('Run simulations (end = 120)... ', appendLF = FALSE)
+sim <- model %>%
+  mrgsim(
+    delta = 1,
+    hmax = 0,
+    maxsteps = 1e9,
+    atol = 1e-7,
+    rtol = 1e-4,
+    end = 120
+  )
+message('OK')
+
+message('Plot results and save to file ${fig_path}... ', appendLF = FALSE)
+plot <- sim %>%
+    plot(A + B + r1 + comp1 ~ time, type='l')
+
+  lattice::trellis.device(device = png, file =  fig_path )
+  print(plot)
+  dev.off()
+message('OK')
