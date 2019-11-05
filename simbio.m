4ea70b1dc19edaaa72fb911b8c729e625d4d50c5
Travis CI <travis@travis-ci.org>
2019-11-05 17:57:54 +0000

    Travis build 59

--- simbio.m
+++ simbio.m
@@ -9,25 +9,25 @@
 sbioaddtolibrary(sbiounit('week', 'day', 7));
 sbioaddtolibrary(sbiounit('xxx', 'nanomole', 1));
 
-one_model = sbiomodel('one');
+undefined_model = sbiomodel('');
 
 % Useful parameters
-addparameter(one_model, 'timeOne', 1, 'ValueUnits', 'hour');
+addparameter(undefined_model, 'timeOne', 1, 'ValueUnits', 'hour');
 
 % Compartments
-one.compartment.comp1 = addcompartment(one_model, 'comp1', 'ConstantCapacity', false, 'Capacity', 1, 'Notes', '', 'Tag', '');
+.compartment.comp1 = addcompartment(undefined_model, 'comp1', 'ConstantCapacity', false, 'Capacity', 1, 'Notes', '', 'Tag', '');
 
 % Species
-one.species.A = addspecies(one.compartment.comp1, 'A', 'ConstantAmount', false, 'InitialAmount', 10, 'BoundaryCondition', false, 'Notes', '', 'Tag', '');
-one.species.B = addspecies(one.compartment.comp1, 'B', 'ConstantAmount', false, 'InitialAmount', 0, 'BoundaryCondition', false, 'Notes', '', 'Tag', '');
+.species.A = addspecies(.compartment.comp1, 'A', 'ConstantAmount', false, 'InitialAmount', 10, 'BoundaryCondition', false, 'Notes', '', 'Tag', '');
+.species.B = addspecies(.compartment.comp1, 'B', 'ConstantAmount', false, 'InitialAmount', 0, 'BoundaryCondition', false, 'Notes', '', 'Tag', '');
 
 % Parameters
-one.parameter.k1 = addparameter(one_model, 'k1', 'ConstantValue', true, 'Value', 0.12, 'Notes', '', 'Tag', '');
+.parameter.k1 = addparameter(undefined_model, 'k1', 'ConstantValue', true, 'Value', 0.12, 'Notes', '', 'Tag', '');
 
 % Reactions
-one.reaction.r1 = addreaction(one_model, 'null -> null', 'Name', 'r1', 'Active', true, 'Reversible', false, 'ReactionRate', 'k1 * A * comp1', 'Notes', '', 'Tag', '');
-  addreactant(one.reaction.r1, [one.species.A, ], [1, ]);
-  addproduct(one.reaction.r1, [one.species.B, ], [2, ]);
+.reaction.r1 = addreaction(undefined_model, 'null -> null', 'Name', 'r1', 'Active', true, 'Reversible', false, 'ReactionRate', 'k1 * A * comp1', 'Notes', '', 'Tag', '');
+  addreactant(.reaction.r1, [.species.A, ], [1, ]);
+  addproduct(.reaction.r1, [.species.B, ], [2, ]);
 
 
 
