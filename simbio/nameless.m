4b1cb396f13981d587a209bd9d3fe40038cb8dd5
metelkin <evgeny.metelkin@gmail.com>
2023-12-27 16:01:17 +0000

    Deploying to gh-pages from @ insysbio/heta-case-mini@5bac36b94d61ac80f8cbf3ab9c0b80882f906b54 🚀

--- simbio/nameless.m
+++ simbio/nameless.m
@@ -4,24 +4,24 @@
   export from : #export { format: Simbio, namespace: nameless, ...};
 %}
 
-sbioaddtolibrary(sbiounit('week', 'day', 7));
+%sbioaddtolibrary(sbiounit('week', 'day', 7));
 
 nameless_model = sbiomodel('nameless');
 
 % Compartments
-nameless.compartment.comp1 = addcompartment(nameless_model, 'comp1', 'ConstantCapacity', false, 'Capacity', 1, 'Notes', '', 'Tag', '');
+nameless_ns.compartment.comp1 = addcompartment(nameless_model, 'comp1', 'ConstantCapacity', false, 'Capacity', 1, 'Notes', '', 'Tag', '');
 
 % Species
-nameless.species.A = addspecies(nameless.compartment.comp1, 'A', 'ConstantAmount', false, 'InitialAmount', 10, 'BoundaryCondition', false, 'Notes', '', 'Tag', '');
-nameless.species.B = addspecies(nameless.compartment.comp1, 'B', 'ConstantAmount', false, 'InitialAmount', 0, 'BoundaryCondition', false, 'Notes', '', 'Tag', '');
+nameless_ns.species.A = addspecies(nameless_ns.compartment.comp1, 'A', 'ConstantAmount', false, 'InitialAmount', 10, 'BoundaryCondition', false, 'Notes', '', 'Tag', '');
+nameless_ns.species.B = addspecies(nameless_ns.compartment.comp1, 'B', 'ConstantAmount', false, 'InitialAmount', 0, 'BoundaryCondition', false, 'Notes', '', 'Tag', '');
 
 % Parameters
-nameless.parameter.k1 = addparameter(nameless_model, 'k1', 'ConstantValue', true, 'Value', 0.12, 'Notes', '', 'Tag', '');
+nameless_ns.parameter.k1 = addparameter(nameless_model, 'k1', 'ConstantValue', true, 'Value', 0.12, 'Notes', '', 'Tag', '');
 
 % Reactions
-nameless.reaction.r1 = addreaction(nameless_model, 'null -> null', 'Name', 'r1', 'Active', true, 'Reversible', false, 'ReactionRate', 'k1 * A * comp1', 'Notes', '', 'Tag', '');
-  addreactant(nameless.reaction.r1, [nameless.species.A, ], [1, ]);
-  addproduct(nameless.reaction.r1, [nameless.species.B, ], [2, ]);
+nameless_ns.reaction.r1 = addreaction(nameless_model, 'null -> null', 'Name', 'r1', 'Active', true, 'Reversible', false, 'ReactionRate', 'k1 * A * comp1', 'Notes', '', 'Tag', '');
+  addreactant(nameless_ns.reaction.r1, [nameless_ns.species.A, ], [1, ]);
+  addproduct(nameless_ns.reaction.r1, [nameless_ns.species.B, ], [2, ]);
 
 
 
@@ -33,5 +33,5 @@ nameless.reaction.r1 = addreaction(nameless_model, 'null -> null', 'Name', 'r1',
 
 
 % Update simulation config
-nameless.config = getconfigset(nameless_model)
-set(nameless.config.SolverOptions, 'AbsoluteToleranceScaling', false)
+nameless_ns.config = getconfigset(nameless_model)
+set(nameless_ns.config.SolverOptions, 'AbsoluteToleranceScaling', false)
